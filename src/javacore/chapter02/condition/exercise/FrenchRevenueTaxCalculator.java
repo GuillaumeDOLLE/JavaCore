a0ec9ffa6621d63ce04c28aa009584191b2a3660
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-03 12:39:45 +0100

    new version with more rules

--- src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
+++ src/javacore/chapter02/condition/exercise/FrenchRevenueTaxCalculator.java
@@ -4,216 +4,135 @@ public class FrenchRevenueTaxCalculator {
 
     public static void main(String[] args) {
 
-        // 5000, 15000, 50000, 125000, 250000 net taxable salary
-        // I could've added for exemple 12000 because the case is interesting to see when we had the deduction
-        double salary = 12000;
+        // 5000, 15000, 50000, 125000, 200000, 250000 gross salary
+        double grossSalary = 15000;
+
+        // employeeType for conditions
+        String employeeType = "Worker"; // or Executive
+        double socialContributionsPercent = 0;
+        double socialContributionsAmount = 0;
+
+        // initialize
+        double netTaxableSalaryAfterSocialContributions = 0;
 
         // BONUS - I add the deduction, I replace the salary by the net taxable salary after deduction in my
         // conditions and my calculations though I let the salary before deduction in prints.
         double netTaxableSalaryAfterDeduction;
-        double deductionPercent = 90.0 / 100;
-        netTaxableSalaryAfterDeduction = salary * deductionPercent;
-        System.out.println(netTaxableSalaryAfterDeduction);
+        double netTaxableSalaryDeductionPercent = 10;
+        double netTaxableSalaryDeductionAmount = 0;
 
-        // taxable rate for each bracket
-        double secondTaxBracketPercent = 89.0 / 100;
-        double thirdTaxBracketPercent = 70.0 / 100;
-        double fourthTaxBracketPercent = 59.0 / 100;
-        double fifthTaxBracketPercent = 55.0 / 100;
+        // taxable percent for each bracket
+        double secondTaxBracketPercent = 11.0;
+        double thirdTaxBracketPercent = 30.0;
+        double fourthTaxBracketPercent = 41.0;
+        double fifthTaxBracketPercent = 45.0;
 
-
-        // highest amount in euros for a full bracket, there probably is a one euro differential with the instructions
-        // because I subtract the upper bound of the previous bracket (I think this is how it's usually done in public
-        // administration, and it is more consistent with the final results).
-        int firstBracketMaxTaxableAmount = 11294;
-        int secondBracketMaxTaxableAmount = 17503;
-        int thirdBracketMaxTaxableAmount = 53544;
-        int fourthBracketMaxTaxableAmount = 94765;
-
-        // Upper bound of taxable brackets, I handle my conditions with these.
+        // Upper bound of taxable brackets, I handle conditions with these.
         int firstBracketUpperBound = 11294;
         int secondBracketUpperBound = 28797;
         int thirdBracketUpperBound = 82341;
         int fourthBracketUpperBound = 177106;
 
-        // highest tax if full bracket
-        int firstTaxBracketFull = 0;
-        double secondTaxBracketFull = 1925.22;
-        double thirdTaxBracketFull = 16062.9;
-        double fourthTaxBracketFull = 38853.24;
-
         // taxable amount per bracket
-        double secondTaxBracket;
-        double thirdTaxBracket;
-        double fourthTaxBracket;
-        double fifthTaxBracket;
+        double secondTaxBracket = 0;
+        double thirdTaxBracket = 0;
+        double fourthTaxBracket = 0;
+        double fifthTaxBracket = 0;
 
         // total taxable amount compared to the net taxable salary
-        double totalTaxAmount;
+        double totalTaxAmount = 0;
 
         // percent of the total  taxable amount compared to the net taxable salary
-        double percentTaxAmount;
+        double percentTaxAmount = 0;
 
-        // variables for display purposes
-        String noTaxesMessage = "With a salary of %.2f euros, there is no taxes.%n";
-        String salaryAndTotalTaxesMessage = "With a salary of %.2f euros, the total amount of income tax is : %.2f euros.%n";
-        String salaryAndPercentTaxesMessage = "With a salary of %.2f euros, the percentage of income tax is : %.2f%%.%n";
+        // socialContributions rule
+        if (employeeType.equals("Worker")) {
 
-        if (netTaxableSalaryAfterDeduction <= firstBracketUpperBound) {
-            System.out.printf(noTaxesMessage, salary);
-        }
-        else if (netTaxableSalaryAfterDeduction <= secondBracketUpperBound) {
+            socialContributionsPercent = 23;
+            socialContributionsAmount = grossSalary * socialContributionsPercent / 100;
+            netTaxableSalaryAfterSocialContributions = grossSalary - socialContributionsAmount;
 
-            // calculation of the taxable amount on the second taxable bracket
-            double secondBracketAmount;
-            secondBracketAmount = netTaxableSalaryAfterDeduction - firstBracketMaxTaxableAmount;
 
-            // calculation of the actual tax on the last taxable bracket
-            secondTaxBracket = secondBracketAmount - (secondBracketAmount * secondTaxBracketPercent);
+        } else if (employeeType.equals("Executive")) {
 
-            // calculation of the taxable amount with a salary higher than 11294 euros
-            totalTaxAmount = firstTaxBracketFull + secondTaxBracket;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
-
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
-
-        }
-        else if (netTaxableSalaryAfterDeduction <= thirdBracketUpperBound) {
-
-            // calculation of the taxable amount on the third taxable bracket
-            double thirdBracketAmount;
-            thirdBracketAmount = netTaxableSalaryAfterDeduction - (firstBracketMaxTaxableAmount + secondBracketMaxTaxableAmount);
-
-            // calculation of the actual tax on the third taxable bracket
-            thirdTaxBracket = thirdBracketAmount - (thirdBracketAmount * thirdTaxBracketPercent);
-
-            // calculation of the taxable amount with a salary higher than 28797 euros
-            totalTaxAmount = firstTaxBracketFull + secondTaxBracketFull + thirdTaxBracket;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
-
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
-
-        }
-        else if (netTaxableSalaryAfterDeduction <= fourthBracketUpperBound) {
-
-            // calculation of the taxable amount on the fourth taxable bracket
-            double fourthBracketAmount;
-            fourthBracketAmount = netTaxableSalaryAfterDeduction - (firstBracketMaxTaxableAmount + secondBracketMaxTaxableAmount + thirdBracketMaxTaxableAmount);
-
-            // calculation of the actual tax on the fourth taxable bracket
-            fourthTaxBracket = fourthBracketAmount - (fourthBracketAmount * fourthTaxBracketPercent);
-
-            // calculation of the taxable amount with a salary higher than 82341 euros
-            totalTaxAmount = firstTaxBracketFull + secondTaxBracketFull + thirdTaxBracketFull + fourthTaxBracket;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
-
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
-
-        }
-        else {
-            // netTaxableSalaryAfterDeduction > fourthBracketUpperBound
-
-            // calculation of the taxable amount on the last taxable bracket
-            double fifthBracketAmount;
-            fifthBracketAmount = netTaxableSalaryAfterDeduction - (firstBracketMaxTaxableAmount + secondBracketMaxTaxableAmount + thirdBracketMaxTaxableAmount + fourthBracketMaxTaxableAmount);
-
-            // calculation of the actual tax on the last taxable bracket
-            fifthTaxBracket  = fifthBracketAmount - (fifthBracketAmount * fifthTaxBracketPercent);
-
-            // calculation of the taxable amount with a salary higher than 177106 euros
-            totalTaxAmount = firstTaxBracketFull + secondTaxBracketFull + thirdTaxBracketFull + fourthTaxBracketFull + fifthTaxBracket;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
-
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
+            socialContributionsPercent = 25;
+            socialContributionsAmount = grossSalary * socialContributionsPercent / 100;
+            netTaxableSalaryAfterSocialContributions = grossSalary - socialContributionsAmount;
 
         }
 
+        // Deduction rule
+        if (netTaxableSalaryAfterSocialContributions >= 100000) {
 
+            netTaxableSalaryDeductionAmount = 10000;
+            netTaxableSalaryAfterDeduction = netTaxableSalaryAfterSocialContributions - netTaxableSalaryDeductionAmount;
 
-        // I did it again only with ifs
+        } else {
 
-        if (netTaxableSalaryAfterDeduction > fourthBracketUpperBound) {
+            netTaxableSalaryDeductionAmount = netTaxableSalaryAfterSocialContributions * netTaxableSalaryDeductionPercent / 100;
+            netTaxableSalaryAfterDeduction = netTaxableSalaryAfterSocialContributions - netTaxableSalaryDeductionAmount;
 
-            // calculation of the taxable amount on the last taxable bracket
+        }
+
+        // copy to keep the base value intact
+        double remainingNetTaxableSalary = netTaxableSalaryAfterDeduction;
+
+        if (remainingNetTaxableSalary > fourthBracketUpperBound) {
+
+            // taxable amount on fifth bracket
             double fifthBracketAmount;
             fifthBracketAmount = netTaxableSalaryAfterDeduction - fourthBracketUpperBound;
 
-            // calculation of the actual tax on the last taxable bracket
-            fifthTaxBracket = fifthBracketAmount - (fifthBracketAmount * fifthTaxBracketPercent);
+            // tax amount
+            fifthTaxBracket = fifthBracketAmount * fifthTaxBracketPercent / 100;
 
-            // calculation of the taxable amount with a salary higher than 177106 euros
-            totalTaxAmount = fifthTaxBracket + fourthTaxBracketFull + thirdTaxBracketFull + secondTaxBracketFull + firstTaxBracketFull;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
+            // subtract remaining salary with the amount taxed in this bracket to go in the next if (cascade)
+            remainingNetTaxableSalary -= fifthBracketAmount;
 
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
         }
 
-        if (netTaxableSalaryAfterDeduction > thirdBracketUpperBound && netTaxableSalaryAfterDeduction <= fourthBracketUpperBound) {
+        if (remainingNetTaxableSalary > thirdBracketUpperBound) {
 
-            // calculation of the taxable amount on the fourth taxable bracket
             double fourthBracketAmount;
-            fourthBracketAmount = netTaxableSalaryAfterDeduction - thirdBracketUpperBound;
+            fourthBracketAmount = remainingNetTaxableSalary - thirdBracketUpperBound;
 
-            // calculation of the actual tax on the fourth taxable bracket
-            fourthTaxBracket = fourthBracketAmount - (fourthBracketAmount * fourthTaxBracketPercent);
+            fourthTaxBracket = fourthBracketAmount * fourthTaxBracketPercent / 100;
 
-            // calculation of the taxable amount with a salary higher than 82341 and lower or equal than 177106 euros
-            totalTaxAmount = fourthTaxBracket + thirdTaxBracketFull + secondTaxBracketFull + firstTaxBracketFull;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
+            remainingNetTaxableSalary -= fourthBracketAmount;
 
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
         }
 
-        if (netTaxableSalaryAfterDeduction > secondBracketUpperBound && netTaxableSalaryAfterDeduction <= thirdBracketUpperBound) {
+        if (remainingNetTaxableSalary > secondBracketUpperBound) {
 
-            // calculation of the taxable amount on the third taxable bracket
             double thirdBracketAmount;
-            thirdBracketAmount = netTaxableSalaryAfterDeduction - secondBracketUpperBound;
+            thirdBracketAmount = remainingNetTaxableSalary - secondBracketUpperBound;
 
-            // calculation of the actual tax on the last taxable bracket
-            thirdTaxBracket = thirdBracketAmount - (thirdBracketAmount * thirdTaxBracketPercent);
+            thirdTaxBracket = thirdBracketAmount * thirdTaxBracketPercent / 100;
 
-            // calculation of the taxable amount with a salary higher than 28797 and lower or equal than 82341 euros
-            totalTaxAmount = thirdTaxBracket + secondTaxBracketFull + firstTaxBracketFull;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
+            remainingNetTaxableSalary -= thirdBracketAmount;
 
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
         }
 
-        if (netTaxableSalaryAfterDeduction > firstBracketUpperBound && netTaxableSalaryAfterDeduction <= secondBracketUpperBound) {
+        if (remainingNetTaxableSalary > firstBracketUpperBound) {
 
-            // calculation of the taxable amount on the third taxable bracket
             double secondBracketAmount;
-            secondBracketAmount = netTaxableSalaryAfterDeduction - firstBracketUpperBound;
+            secondBracketAmount = remainingNetTaxableSalary - firstBracketUpperBound;
 
-            // calculation of the actual tax on the last taxable bracket
-            secondTaxBracket = secondBracketAmount - (secondBracketAmount * secondTaxBracketPercent);
+            secondTaxBracket = secondBracketAmount * secondTaxBracketPercent / 100;
 
-            // calculation of the taxable amount with a salary higher than 11294 and lower or equal than 28797 euros
-            totalTaxAmount = secondTaxBracket + firstTaxBracketFull;
-            System.out.printf(salaryAndTotalTaxesMessage, salary, totalTaxAmount);
-
-            // percent of total taxes compared to the net taxable salary
-            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
-            System.out.printf(salaryAndPercentTaxesMessage, salary, percentTaxAmount);
         }
 
-        if (netTaxableSalaryAfterDeduction <= firstBracketUpperBound) {
-            System.out.printf(noTaxesMessage, salary);
+        if (netTaxableSalaryAfterDeduction > firstBracketUpperBound) {
+
+            // either that or totalTaxAmount += taxBracket for each if
+            totalTaxAmount = fifthTaxBracket + fourthTaxBracket + thirdTaxBracket + secondTaxBracket;
+            percentTaxAmount = totalTaxAmount * 100 / netTaxableSalaryAfterDeduction;
+            System.out.printf("With a net taxable salary after deduction of %.2f euros, the total amount of income tax is : %.2f euros.%nThe percentage of this income tax is %.2f%%.%n", netTaxableSalaryAfterDeduction, totalTaxAmount, percentTaxAmount);
+
+        } else {
+
+            System.out.printf("With a salary of %.2f euros, you have no taxes to pay.%n", netTaxableSalaryAfterDeduction);
+
         }
 
     }
