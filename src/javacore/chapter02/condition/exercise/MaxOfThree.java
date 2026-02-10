bb605426480c975e32528dc9b87ad09cc40c7efd
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-10 09:37:16 +0100

    bonus

--- src/javacore/chapter02/condition/exercise/MaxOfThree.java
+++ src/javacore/chapter02/condition/exercise/MaxOfThree.java
@@ -4,15 +4,31 @@ public class MaxOfThree {
 
     public static void main(String[] args) {
 
-        int a = 3;
-        int b = 22;
-        int c = 111;
+        int a = 333;
+        int b = 403;
+        int c = 33;
 
-        if (a > b && a > c) {
+        if (a == b || a == c || b == c) {
+            if (a == b && b == c) {
+                System.err.println("Toutes les valeurs sont égales, il n'y en a pas une plus grande que les autres.");
+            }
+            else if (a == b) {
+                System.err.println("Les valeurs de a et b sont égales.");
+            }
+            else if (b == c) {
+                System.err.println("Les valeurs de b et c sont égales.");
+            }
+            else {
+                System.err.println("Les valeurs de a et c sont égales.");
+            }
+        }
+        else if (a > b && a > c) {
             System.out.println("a est plus grand que b et c");
-        } else if (b > a && b > c) {
+        }
+        else if (b > a && b > c) {
             System.out.println("b est plus grand que a et c");
-        } else {
+        }
+        else {
             System.out.println("c est plus grand que a et b");
         }
     }
