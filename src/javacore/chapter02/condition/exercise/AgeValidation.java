56c70587ed63272bff090081914122a67bfea4f7
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-10 14:50:00 +0100

    bonus 2 and 3

--- src/javacore/chapter02/condition/exercise/AgeValidation.java
+++ src/javacore/chapter02/condition/exercise/AgeValidation.java
@@ -21,9 +21,9 @@ public class AgeValidation {
          * Fin d'un code que nous ne comprenons pas, mais dont nous pouvons utiliser les variables !
          */
 
-        int userBirthDay = 11; // < 10, 10, > 10
-        int userBirthMonth = 5; // < 2, 2, > 2
-        int userBirthYear = 2007; // < 2008, 2008, > 2008
+        int userBirthDay = 22; // < 10, 10, > 10
+        int userBirthMonth = 3; // < 2, 2, > 2
+        int userBirthYear = 2008; // < 2008, 2008, > 2008
 
         if ((currentYear - userBirthYear > 18) || (currentYear - userBirthYear == 18) && (userBirthMonth < currentMonth || (userBirthMonth == currentMonth && userBirthDay <= currentDay))) {
             System.out.println("You are an adult.");
@@ -45,11 +45,15 @@ public class AgeValidation {
                     System.out.println("You are an adult.");
                 }
                 else {
-                    System.out.println("You are a minor.");
+                    int daysUntilAdult = userBirthDay - currentDay;
+                    System.out.printf("You are a minor. You will be an adult in %d days.%n", daysUntilAdult);
                 }
             }
             else {
-                System.out.println("You are a minor.");
+                int monthsConvertedInDaysUntilAdult = (userBirthMonth - currentMonth) * 30;
+                int daysUntilAdult = userBirthDay - currentDay;
+                int timeUntilAdult = daysUntilAdult + monthsConvertedInDaysUntilAdult;
+                System.out.printf("You are a minor. You will be an adult in %d days.%n", timeUntilAdult);
             }
         }
         else {
@@ -62,11 +66,15 @@ public class AgeValidation {
         }
         else if (currentYear - userBirthYear == 18) {
             if (userBirthMonth > currentMonth) {
-                System.out.println("You are a minor.");
+                int monthsConvertedInDaysUntilAdult = (userBirthMonth - currentMonth) * 30;
+                int daysUntilAdult = userBirthDay - currentDay;
+                int timeUntilAdult = daysUntilAdult + monthsConvertedInDaysUntilAdult;
+                System.out.printf("You are a minor. You will be an adult in %d days.%n", timeUntilAdult);
             }
             else if (userBirthMonth == currentMonth) {
                 if (userBirthDay > currentDay) {
-                    System.out.println("You are a minor.");
+                    int daysUntilAdult = userBirthDay - currentDay;
+                    System.out.printf("You are a minor. You will be an adult in %d days.%n", daysUntilAdult);
                 }
                 else {
                     System.out.println("You are an adult.");
@@ -79,6 +87,12 @@ public class AgeValidation {
         else {
             System.out.println("You are an adult.");
         }
+
+        // Instructions impacted by leap years : no impact because I already use the civil calendar which implement leap years
+        // The only issue I see that might happen is if someone is born on a leap year the 29th of february.
+        // If that happens I think the right way to handle this is to consider the end of the year to be the 1st of march.
+
+
     }
 
 }
