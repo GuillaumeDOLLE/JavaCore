73a41f3fa702ef8c50c34db880275bab8b06326c
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-10 11:27:50 +0100

    first attempt before bonus

---
+++ src/javacore/chapter02/condition/exercise/AgeValidation.java
@@ -0,0 +1,39 @@
+package javacore.chapter02.condition.exercise;
+
+public class AgeValidation {
+
+    public static void main(String[] args) {
+
+        /**
+         * Début d'un code que nous ne comprenons pas à ce stade de la formation...
+         */
+
+        // Récupération de la date courante de la machine exécutant ce programme
+        java.time.LocalDate currentDate = java.time.LocalDate.now();
+
+        int currentDay   = currentDate.getDayOfMonth(); // Jour courant (entre 1 et 31)
+        int currentMonth = currentDate.getMonthValue(); // Mois courant (entre 1 et 12)
+        int currentYear  = currentDate.getYear();       // Année courante (202X)
+
+        System.out.println("Nous somme le " + currentDay + "/" + currentMonth + "/" + currentYear);
+
+        /**
+         * Fin d'un code que nous ne comprenons pas, mais dont nous pouvons utiliser les variables !
+         */
+
+        int userBirthDay = 1; // < 10, 10, > 10
+        int userBirthMonth = 2; // < 2, 2, > 2
+        int userBirthYear = 2008; // < 2008, 2008, > 2008
+
+        // 30 < 10, 12 < 2, 2026 - 2008 = 18
+
+        if ((userBirthDay <= currentDay && userBirthMonth <= currentMonth) || (userBirthDay > currentDay && userBirthMonth < currentMonth) && (currentYear - userBirthYear) >= 18) {
+            System.out.println("You are an adult.");
+        }
+        else {
+            System.out.println("You are a minor.");
+        }
+
+    }
+
+}
