68f95cfdca62c8ddb29d7c2080f63f15da338528
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-16 16:42:55 +0100

    second attempt after review, conversion in seconds to be more precise

--- src/javacore/chapter02/condition/exercise/AgeValidation.java
+++ src/javacore/chapter02/condition/exercise/AgeValidation.java
@@ -1,5 +1,8 @@
 package javacore.chapter02.condition.exercise;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class AgeValidation {
 
     public static void main(String[] args) {
@@ -21,77 +24,92 @@ public class AgeValidation {
          * Fin d'un code que nous ne comprenons pas, mais dont nous pouvons utiliser les variables !
          */
 
-        int userBirthDay = 22; // < 10, 10, > 10
-        int userBirthMonth = 3; // < 2, 2, > 2
-        int userBirthYear = 2008; // < 2008, 2008, > 2008
+        // Inputs
+        final int USER_BIRTH_DAY = 18; // < 10, 10, > 10
+        final int USER_BIRTH_MONTH = 2; // < 2, 2, > 2
+        final int USER_BIRTH_YEAR = 2008; // < 2008, 2008, > 2008
 
-        if ((currentYear - userBirthYear > 18) || (currentYear - userBirthYear == 18) && (userBirthMonth < currentMonth || (userBirthMonth == currentMonth && userBirthDay <= currentDay))) {
+        final int MONTHS_PER_YEAR = 12;
+        int daysAverageMonth = 30;
+        final int HOURS_PER_DAY = 24;
+        final int MINUTES_PER_HOUR = 60;
+        final int SECONDS_PER_MINUTE = 60;
+
+        final int ADULT_IN_YEARS = 18;
+
+        // Conversion
+        int daysConvertedInSeconds = HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE;
+        int monthsConvertedInSeconds = daysConvertedInSeconds * daysAverageMonth;
+        long yearsConvertedInSeconds = monthsConvertedInSeconds * MONTHS_PER_YEAR;
+
+
+        int userBirthDayInSeconds = USER_BIRTH_DAY * daysConvertedInSeconds;
+        int userBirthMonthInSeconds = USER_BIRTH_MONTH * monthsConvertedInSeconds;
+        long userBirthYearInSeconds = USER_BIRTH_YEAR * yearsConvertedInSeconds;
+        long userBirthSeconds = userBirthDayInSeconds + userBirthMonthInSeconds + userBirthYearInSeconds;
+
+
+        int currentDaysConvertedInSeconds = currentDay * daysConvertedInSeconds;
+        int currentMonthsConvertedInSeconds = currentMonth * monthsConvertedInSeconds;
+        long currentYearsConvertedInSeconds = currentYear * yearsConvertedInSeconds;
+        long currentSeconds = currentDaysConvertedInSeconds + currentMonthsConvertedInSeconds + currentYearsConvertedInSeconds;
+
+        // calc
+        long secondsSinceUserBirthday = currentSeconds - userBirthSeconds;
+
+        long majorityInSeconds = ADULT_IN_YEARS * yearsConvertedInSeconds;
+        System.out.println("seconds for someone to be a young adult : " + majorityInSeconds);
+
+        // 2nd version with seconds conversion before calc
+        if (secondsSinceUserBirthday < majorityInSeconds) {
+            long secondsUntilAdult = majorityInSeconds - secondsSinceUserBirthday;
+            // output
+            System.out.printf("You are a minor, Only %d seconds left to be an adult !%n", secondsUntilAdult);
+        }
+        else {
+            System.out.println("You are an adult.");
+        }
+
+        // 1st version
+        if ((currentYear - USER_BIRTH_YEAR > 18) || (currentYear - USER_BIRTH_YEAR == 18) && (USER_BIRTH_MONTH < currentMonth || (USER_BIRTH_MONTH == currentMonth && USER_BIRTH_DAY <= currentDay))) {
             System.out.println("You are an adult.");
         }
         else {
             System.out.println("You are a minor.");
         }
 
-        // Nested if - adult
-        if (currentYear - userBirthYear > 18) {
+        // 1st version with seconds conversion after calc
+        if (currentYear - USER_BIRTH_YEAR < 18) {
+            System.out.println("You are a minor.");
+        }
+        else if (currentYear - USER_BIRTH_YEAR > 18) {
             System.out.println("You are an adult.");
         }
-        else if (currentYear - userBirthYear == 18) {
-            if (userBirthMonth < currentMonth) {
+        else {
+            if (USER_BIRTH_MONTH > currentMonth) {
+                int monthsConvertedInDaysUntilAdult = (USER_BIRTH_MONTH - currentMonth) * daysAverageMonth;
+                int daysUntilAdult = USER_BIRTH_DAY - currentDay;
+                int daysConvertedInSecondsUntilAdult = monthsConvertedInDaysUntilAdult * daysConvertedInSeconds + daysUntilAdult * daysConvertedInSeconds;
+                System.out.printf("You are a minor. You will be an adult in %d seconds.%n", daysConvertedInSecondsUntilAdult);
+            }
+            else if (USER_BIRTH_MONTH < currentMonth) {
                 System.out.println("You are an adult.");
             }
-            else if (userBirthMonth == currentMonth) {
-                if (userBirthDay <= currentDay) {
-                    System.out.println("You are an adult.");
-                }
-                else {
-                    int daysUntilAdult = userBirthDay - currentDay;
-                    System.out.printf("You are a minor. You will be an adult in %d days.%n", daysUntilAdult);
-                }
-            }
             else {
-                int monthsConvertedInDaysUntilAdult = (userBirthMonth - currentMonth) * 30;
-                int daysUntilAdult = userBirthDay - currentDay;
-                int timeUntilAdult = daysUntilAdult + monthsConvertedInDaysUntilAdult;
-                System.out.printf("You are a minor. You will be an adult in %d days.%n", timeUntilAdult);
-            }
-        }
-        else {
-            System.out.println("You are a minor.");
-        }
-
-        // Nested if - minor
-        if (currentYear - userBirthYear < 18) {
-            System.out.println("You are a minor.");
-        }
-        else if (currentYear - userBirthYear == 18) {
-            if (userBirthMonth > currentMonth) {
-                int monthsConvertedInDaysUntilAdult = (userBirthMonth - currentMonth) * 30;
-                int daysUntilAdult = userBirthDay - currentDay;
-                int timeUntilAdult = daysUntilAdult + monthsConvertedInDaysUntilAdult;
-                System.out.printf("You are a minor. You will be an adult in %d days.%n", timeUntilAdult);
-            }
-            else if (userBirthMonth == currentMonth) {
-                if (userBirthDay > currentDay) {
-                    int daysUntilAdult = userBirthDay - currentDay;
-                    System.out.printf("You are a minor. You will be an adult in %d days.%n", daysUntilAdult);
+                if (USER_BIRTH_DAY > currentDay) {
+                    int daysUntilAdult = USER_BIRTH_DAY - currentDay;
+                    int daysConvertedInSecondsUntilAdult = daysUntilAdult * HOURS_PER_DAY * MINUTES_PER_HOUR * SECONDS_PER_MINUTE;
+                    System.out.printf("You are a minor. You will be an adult in %d seconds.%n", daysConvertedInSecondsUntilAdult);
                 }
                 else {
                     System.out.println("You are an adult.");
                 }
             }
-            else {
-                System.out.println("You are an adult.");
-            }
-        }
-        else {
-            System.out.println("You are an adult.");
         }
 
         // Instructions impacted by leap years : no impact because I already use the civil calendar which implement leap years
         // The only issue I see that might happen is if someone is born on a leap year the 29th of february.
         // If that happens I think the right way to handle this is to consider the end of the year to be the 1st of march.
-
 
     }
 
