991fe4ebf4329350fce09fd52626780a0927545b
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-23 15:41:55 +0100

    Bonus with 1000 terms + even count

--- src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
+++ src/javacore/chapter03/loop/exercise/FibonacciCalculator.java
@@ -1,28 +1,42 @@
 package javacore.chapter03.loop.exercise;
 
+import java.math.BigDecimal;
+
 public class FibonacciCalculator {
 
     public static void main(String[] args) {
 
-        // F(n) = F(n-1) + F(n-2)
+        // init values
+        final BigDecimal FIRST_TERM = new BigDecimal("0");
+        final BigDecimal SECOND_TERM = new BigDecimal("1");
+        System.out.println("The term F(0) of the Fibonacci sequence is : " + FIRST_TERM);
+        System.out.println("The term F(1) of the Fibonacci sequence is : " + SECOND_TERM);
 
-        long previousTerm = 0; // F(n-2)
-        long currentTerm = 1;  // F(n-1)
+        // copy F(0) & F(1) to find F(2)
+        BigDecimal previousTerm = FIRST_TERM;
+        BigDecimal currentTerm = SECOND_TERM;
 
-        int limitNumber = 50;
+        int limitNumber = 1000;
+        int evenTermsCount = 0;
 
-        // loop through all from 2 to 50 (already have 0 and 1)
+        // loop through all from countTerm to limitNumber (already have the 2 first terms)
         for (int countTerm = 2 ; countTerm < limitNumber ; countTerm++) {
 
-            System.out.println("countTerm = " + countTerm);
+            // F(n) = F(n-1) + F(n-2)
+            BigDecimal nextTerm = previousTerm.add(currentTerm);
 
-            long nextTerm = previousTerm + currentTerm; // F(n) = F(n-1) + F(n-2)
-            System.out.println("next " + nextTerm);
+            // Check even terms
+            BigDecimal checkEvenDivisor = new BigDecimal("2");
+            // number % 2 == 0 ? yes even / no odd
+            if (nextTerm.remainder(checkEvenDivisor).equals(BigDecimal.ZERO)) {
+                evenTermsCount++;
+            }
 
-            previousTerm = currentTerm; // F(n-2) becomes F(n-1) after a turn
-            currentTerm = nextTerm; // F(n-1) becomes F(n) after a turn
-
+            System.out.printf("The term F(%d) of the fibonacci sequence is : %.0f.%n", countTerm, nextTerm);
+            previousTerm = currentTerm;
+            currentTerm = nextTerm;
         }
+        System.out.printf("There is %d even terms from F(0) to F(999).%n", evenTermsCount);
     }
 
 }
