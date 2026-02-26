5722242ce47e967931be583e1d1316a7445987c6
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-26 16:36:43 +0100

    first attempt

---
+++ src/javacore/chapter03/loop/exercise/FullPyramid.java
@@ -0,0 +1,23 @@
+package javacore.chapter03.loop.exercise;
+
+public class FullPyramid {
+
+    public static void main(String[] args) {
+
+        int numberOfRows = 12;
+
+        for (int currentRow = 1; currentRow <= numberOfRows ; currentRow++) {
+            System.out.println();
+
+            for (int numberOfSpace = numberOfRows - currentRow ; numberOfSpace > 0 ; numberOfSpace--) {
+                System.out.print(" ");
+            }
+            for (int numberOfAsterisk = 1 ; numberOfAsterisk <= 2 * currentRow - 1; numberOfAsterisk++) {
+                System.out.print("*");
+            }
+        }
+
+    }
+
+}
+
