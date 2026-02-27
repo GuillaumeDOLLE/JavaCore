4cbdaf43db7132d3fa94d47d832517db319631cf
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-02-27 11:43:33 +0100

    refactor after review, removed all useless ifs

--- src/javacore/chapter03/loop/exercise/MultiplicationTables.java
+++ src/javacore/chapter03/loop/exercise/MultiplicationTables.java
@@ -11,44 +11,8 @@ public class MultiplicationTables {
             System.out.print("Table de " + table + " : ");
 
             for (int count = 1 ; count <= multiply ; count++) {
-
-                if (table == 1) {
                     int result = table * count;
                     System.out.print(result + " ");
-
-                }
-                if (table == 2) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 3) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 4) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 5) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 6) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 7) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 8) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
-                if (table == 9) {
-                    int result = table * count;
-                    System.out.print(result + " ");
-                }
             }
 
             System.out.println();
