2a1ee1f8178f5d76cd49e539c5a4e35a7b834708
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-09 12:24:21 +0100

    second attempt

--- src/javacore/chapter04/function/exercise/project/refactor/PerfectNumberFinderFunctionRefactor.java
+++ src/javacore/chapter04/function/exercise/project/refactor/PerfectNumberFinderFunctionRefactor.java
@@ -2,45 +2,35 @@ package javacore.chapter04.function.exercise.project.refactor;
 
 public class PerfectNumberFinderFunctionRefactor {
 
-    // TODO: refactor this
-    public static int findAndAddAllDivisors(int cPotentialPerfectNumber) {
-        // reset sum to 0 for each turn
+    public static void displayPerfectNumber(int cPotentialPerfectNumber) {
+        System.out.printf("%d is a perfect number.%n", cPotentialPerfectNumber);
+    }
+
+    public static boolean isPerfectNumber(int cPotentialPerfectNumber) {
         int sumDivisors = 0;
-        // in the condition of the loop, I check if sum is already higher so there is no point to continue
-        for (int potentialDivisor = 1 ; potentialDivisor < cPotentialPerfectNumber && sumDivisors <= cPotentialPerfectNumber ; potentialDivisor++) {
-            if (cPotentialPerfectNumber % potentialDivisor == 0) {
-                // Here the potentialDivisor is a real divisor, so I add it to the sum of divisors
-                sumDivisors += potentialDivisor;
+
+        for (int PotentialDivisor = 1; PotentialDivisor < cPotentialPerfectNumber && sumDivisors <= cPotentialPerfectNumber; PotentialDivisor++) {
+            if (cPotentialPerfectNumber % PotentialDivisor == 0) {
+                sumDivisors += PotentialDivisor;
             }
         }
-        // return sumDivisors to use it to compare it with the potentialPerfectNumber in another function
-        return sumDivisors;
-    }
 
-    public static int loopThroughAllPotentialPerfectNumbers(int start, int end) {
-        int foundPerfectNumber = 0;
-        for (int cPotentialPerfectNumber = start ; cPotentialPerfectNumber <= end ; cPotentialPerfectNumber++) {
-            int sumDivisors = findAndAddAllDivisors(cPotentialPerfectNumber);
-
-            foundPerfectNumber = displayPerfectNumber(cPotentialPerfectNumber, sumDivisors, foundPerfectNumber);
-        }
-        return foundPerfectNumber;
-    }
-
-    public static int displayPerfectNumber(int cPotentialPerfectNumber, int sumDivisors, int foundPerfectNumber) {
-        if (sumDivisors == cPotentialPerfectNumber) {
-            foundPerfectNumber++;
-            System.out.printf("%d is a perfect number.%n", cPotentialPerfectNumber);
-        }
-        return foundPerfectNumber;
+        return sumDivisors == cPotentialPerfectNumber;
     }
 
     public static void main(String[] args) {
 
         int start = 2;
         int end = 100000;
+        int foundPerfectNumber = 0;
 
-        int foundPerfectNumber = loopThroughAllPotentialPerfectNumbers(start, end);
+        for (int cPotentialPerfectNumber = start; cPotentialPerfectNumber <= end; cPotentialPerfectNumber++) {
+            if (isPerfectNumber(cPotentialPerfectNumber)) {
+                foundPerfectNumber++;
+                displayPerfectNumber(cPotentialPerfectNumber);
+            }
+        }
+
         System.out.printf("There is %d perfect numbers between %d and %d.%n", foundPerfectNumber, start, end);
     }
 
