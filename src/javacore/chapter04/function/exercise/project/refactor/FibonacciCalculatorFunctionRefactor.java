a16fd84a8cb0fc5ee1e9626f75da7068b8544cb0
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-11 11:46:39 +0100

    oopsie, fix variable -> const

--- src/javacore/chapter04/function/exercise/project/refactor/FibonacciCalculatorFunctionRefactor.java
+++ src/javacore/chapter04/function/exercise/project/refactor/FibonacciCalculatorFunctionRefactor.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class FibonacciCalculatorFunctionRefactor {
 
-    public static BigDecimal EVEN_DIVISOR = new BigDecimal("2");
+    public static final BigDecimal EVEN_DIVISOR = new BigDecimal("2");
 
     public static BigDecimal computeNextTerm(BigDecimal previousTerm, BigDecimal currentTerm) {
         return previousTerm.add(currentTerm);
