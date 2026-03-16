58341819529a52935a1ddfc94a243e6f2fdd6175
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-16 12:37:17 +0100

    first attempt

---
+++ src/javacore/chapter04/function/exercise/RecursiveFactorial.java
@@ -0,0 +1,34 @@
+package javacore.chapter04.function.exercise;
+
+public class RecursiveFactorial {
+
+    public static long getFactorial(int number, long result) {
+
+        result += result * (number - 1);
+        number--;
+
+        if (number <= 1) {
+            return result;
+        }
+        return getFactorial(number, result);
+    }
+
+    public static void main(String[] args) {
+
+        int number = 6;
+        int tempNumber = number;
+        long result = 1;
+
+        if (tempNumber < 0) {
+            System.out.println("You can't calculate the factorial number if it is negative.");
+        }
+        else if (tempNumber == 0) {
+            System.out.println("The factorial number of 0 is automatically 1.");
+        }
+        else {
+            result = getFactorial(tempNumber, result);
+            System.out.printf("The factorial of %d is : %d.%n", number, result);
+        }
+    }
+
+}
