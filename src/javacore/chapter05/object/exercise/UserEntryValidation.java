651299bbef42efcce1b48db1f86abfcd0edc0f08
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-19 10:47:11 +0100

    first instructions, user entry validation with do while ok

---
+++ src/javacore/chapter05/object/exercise/UserEntryValidation.java
@@ -0,0 +1,20 @@
+package javacore.chapter05.object.exercise;
+
+import java.util.Scanner;
+
+public class UserEntryValidation {
+
+    public static void main(String[] args) {
+
+        String today;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entrez le jour de la semaine : ");
+        do {
+            today = scan.next();
+        } while (!today.equals("jeudi"));
+
+    }
+
+}
