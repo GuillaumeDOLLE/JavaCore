12187e5cb2e44601923fce15e0c28ee6eef5980f
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-20 15:24:36 +0100

    first attempt with loops and condition

---
+++ src/javacore/chapter05/object/exercise/AlphabetVowelSearcher.java
@@ -0,0 +1,21 @@
+package javacore.chapter05.object.exercise;
+
+public class AlphabetVowelSearcher {
+
+    public static void main(String[] args) {
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String vowels = "aeiouy";
+
+        for (int index = 0; index < alphabet.length(); index++) {
+            char cr = alphabet.charAt(index);
+            for (int i = 0; i < vowels.length(); i++) {
+                if (cr == vowels.charAt(i)) {
+                    System.out.println("Voyelle trouvée : " + cr + " à la position : " + index);
+                }
+            }
+        }
+
+    }
+
+}
