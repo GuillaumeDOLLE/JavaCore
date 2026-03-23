249ff944f142e7c441fbd97e1fa04a9027903378
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-23 15:19:48 +0100

    third instruction, vowel counter with indexOf(char, fromIndex)

--- src/javacore/chapter05/object/exercise/VowelCounter.java
+++ src/javacore/chapter05/object/exercise/VowelCounter.java
@@ -7,47 +7,30 @@ public class VowelCounter {
     public static void main(String[] args) {
 
         String vowels = "aeiouy";
-        int vowelsACounter = 0;
-        int vowelsECounter = 0;
-        int vowelsICounter = 0;
-        int vowelsOCounter = 0;
-        int vowelsUCounter = 0;
-        int vowelsYCounter = 0;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Veuillez saisir un texte : ");
-        String userEntryText = scanner.nextLine().trim().toLowerCase();
+        String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        String loremIpsumLowerCasedAndTrimmed = loremIpsum.toLowerCase().trim();
 
-        for (int index = 0; index < userEntryText.length(); index++) {
-            char cr = userEntryText.charAt(index);
-            for (int i = 0; i < vowels.length(); i++) {
-                if (cr == vowels.charAt(i)) {
-                    if (cr == 'a') {
-                        vowelsACounter++;
-                    }
-                    if (cr == 'e') {
-                        vowelsECounter++;
-                    }
-                    if (cr == 'i') {
-                        vowelsICounter++;
-                    }
-                    if (cr == 'o') {
-                        vowelsOCounter++;
-                    }
-                    if (cr == 'u') {
-                        vowelsUCounter++;
-                    }
-                    if (cr == 'y') {
-                        vowelsYCounter++;
-                    }
+        System.out.println("Voici le compteur de chaque voyelle présent dans le texte :");
+        // Instead of looping through all the text, I only look for the vowels directly
+        for (int index = 0; index < vowels.length(); index++) {
+            char vowel = vowels.charAt(index);
+            int countVowel = 0;
+            // Get index of the first vowel
+            int vowelIndex = loremIpsumLowerCasedAndTrimmed.indexOf(vowel);
 
-                }
+            // Different from -1 so basically is there a vowel ?
+            while (vowelIndex != -1) {
+                countVowel++;
+                // Update index based on last vowel index found
+                vowelIndex = loremIpsumLowerCasedAndTrimmed.indexOf(vowel, vowelIndex + 1);
+
             }
+
+            System.out.println("- " + vowel + " = " + countVowel);
+
         }
 
-        System.out.println("Voici le compteur de chaque voyelle :\n- a = " + vowelsACounter + "\n- e = "
-                + vowelsECounter + "\n- i = " + vowelsICounter + "\n- o = " + vowelsOCounter + "\n- u = " +
-                vowelsUCounter + "\n- y = " + vowelsYCounter);
 
     }
 
