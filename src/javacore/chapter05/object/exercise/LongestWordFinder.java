8746f73a638dbfb2ed0f436524ea23fa5ef292d3
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-24 12:11:52 +0100

    third instruction, find longest word without the use of String.split

--- src/javacore/chapter05/object/exercise/LongestWordFinder.java
+++ src/javacore/chapter05/object/exercise/LongestWordFinder.java
@@ -5,16 +5,35 @@ public class LongestWordFinder {
     public static void main(String[] args) {
 
         String text = "a bb ccc ddddddddddd eeeeeeeeee fffffffff gggg hhhhh iiiiiiiiiiii jjjjjj kkkkkkk mmmmmmmm";
-        String regex = " ";
-        String[] arrayTexts = text.split(regex);
 
-        for (String str : arrayTexts) {
-            int countChar = 0;
-            for (int index = 0; index < str.length(); index++) {
-                countChar++;
+        String longestWord = "";
+        String currentWord = "";
+        char space = ' ';
+
+        // loop text
+        for (int charIndex = 0; charIndex < text.length(); charIndex++) {
+
+            // get char with index
+            char currentChar = text.charAt(charIndex);
+
+            // check currentChar
+            if (currentChar != space) {
+                // build word
+                currentWord += currentChar;
             }
-            System.out.println(str + " : " + countChar);
+            else {
+                // new word after the space
+                currentWord = "";
+            }
+
+            // check length here to acknowledge the last word even with no space after it
+            if (currentWord.length() > longestWord.length()) {
+                longestWord = currentWord;
+            }
+
         }
+
+        System.out.println("The longest word in the text is : \"" + longestWord + "\" it has " + longestWord.length() + " characters.");
 
     }
 
