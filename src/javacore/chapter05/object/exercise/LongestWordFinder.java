c57490e14eb7a857d3f1c0cfa3bdbcfb7732c9d4
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-25 16:02:36 +0100

    correction after review

--- src/javacore/chapter05/object/exercise/LongestWordFinder.java
+++ src/javacore/chapter05/object/exercise/LongestWordFinder.java
@@ -9,7 +9,7 @@ public class LongestWordFinder {
         String longestWord = "";
         String currentWord = "";
         char space = ' ';
-
+        System.out.println(text.length());
         // loop text
         for (int charIndex = 0; charIndex < text.length(); charIndex++) {
 
@@ -17,24 +17,37 @@ public class LongestWordFinder {
             char currentChar = text.charAt(charIndex);
 
             // check currentChar
-            if (currentChar != space) {
-                // build word
-                currentWord += currentChar;
-            }
-            else {
+            if (currentChar == space) {
+                if (currentWord.length() > longestWord.length()) {
+                    longestWord = currentWord;
+                }
                 // new word after the space
                 currentWord = "";
             }
+            else {
+                // build word
+                currentWord += currentChar;
+                // check length here to acknowledge the last word of the text
+                if (charIndex == text.length() - 1 && currentWord.length() > longestWord.length()) {
+                    longestWord = currentWord;
+                }
 
-            // check length here to acknowledge the last word even with no space after it
-            if (currentWord.length() > longestWord.length()) {
-                longestWord = currentWord;
             }
-
         }
 
         System.out.println("The longest word in the text is : \"" + longestWord + "\" it has " + longestWord.length() + " characters.");
 
+//        String text = "a bb ccc ddddddddddd eeeeeeeeee fffffffff gggg hhhhh iiiiiiiiiiii jjjjjj kkkkkkk mmmmmmmm";
+//        String regex = " ";
+//        String[] arrayTexts = text.split(regex);
+//        String longestWord = "";
+//
+//        for (String str : arrayTexts) {
+//            if (str.length() > longestWord.length()) {
+//                longestWord = str;
+//            }
+//        }
+//        System.out.println("The longest word in the text is : \"" + longestWord + "\" it has " + longestWord.length() + " characters.");
     }
 
 }
