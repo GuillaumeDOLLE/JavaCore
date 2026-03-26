2d0a638445d104ef4b55a54a52e905bb2b5affca
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-26 11:25:47 +0100

    second instruction, replaceAll to encrypt the text

--- src/javacore/chapter05/object/project/SubstitutionCipher.java
+++ src/javacore/chapter05/object/project/SubstitutionCipher.java
@@ -23,28 +23,55 @@ public class SubstitutionCipher {
         // Ecrivez le code ci-dessous
         char currentChar = ' ';
         char currentSubChar = ' ';
+//
+//        String textEncrypted = "";
+//
+//        // loop text
+//        for (int textIndex = 0; textIndex < textToEncrypt.length(); textIndex++) {
+//            // find each char
+//            currentChar = textToEncrypt.charAt(textIndex);
+//
+//            if (currentChar == ' ' || currentChar == '!') {
+//                currentSubChar = currentChar;
+//            }
+//            else {
+//                int currentCharIndex = 0;
+//                // find char index from latin alphabet
+//                currentCharIndex = latinAlphabet.indexOf(currentChar);
+//                // find sub char with latin alphabet index
+//                currentSubChar = substitutionAlphabet.charAt(currentCharIndex);
+//            }
+//            // Rebuilding of the text from each substitution char
+//            textEncrypted += currentSubChar;
+//        }
 
-        String textEncrypted = "";
 
-        // loop text
-        for (int textIndex = 0; textIndex < textToEncrypt.length(); textIndex++) {
-            // find each char
-            currentChar = textToEncrypt.charAt(textIndex);
+        String currentLatinLetter = "";
+        String currentSubLetter = "";
+        String newTextToEncrypt = textToEncrypt;
 
-            if (currentChar == ' ' || currentChar == '!') {
-                currentSubChar = currentChar;
-            }
-            else {
-                int currentCharIndex = 0;
-                // find char index from latin alphabet
-                currentCharIndex = latinAlphabet.indexOf(currentChar);
-                // find sub char with latin alphabet index
-                currentSubChar = substitutionAlphabet.charAt(currentCharIndex);
-            }
-            // Rebuilding of the text from each substitution char
-            textEncrypted += currentSubChar;
+        for (int i = 0; i < latinAlphabet.length(); i++) {
+            currentChar = latinAlphabet.charAt(i);
+
+            currentLatinLetter = "" + currentChar;
+
+            newTextToEncrypt = newTextToEncrypt.replaceAll(currentLatinLetter, "_" + i + "_");
+
+        }
+        // first encrypt
+        System.out.println("text encrypted : " + newTextToEncrypt);
+
+        String textEncrypted = newTextToEncrypt;
+
+        for (int i = 0; i < substitutionAlphabet.length(); i++) {
+            currentSubChar = substitutionAlphabet.charAt(i);
+
+            currentSubLetter = "" + currentSubChar;
+
+            textEncrypted = textEncrypted.replaceAll("_" + i + "_", currentSubLetter);
         }
 
+        // second encrypt to exclude overlapping replacements
         System.out.println("Final encrypted text : " + textEncrypted);
     }
 
