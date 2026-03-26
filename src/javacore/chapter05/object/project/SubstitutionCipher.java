5bfd42e28c899aadf96bf5c96fcbd6975f7efeb4
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-03-26 11:37:47 +0100

    third instruction, cipher method

--- src/javacore/chapter05/object/project/SubstitutionCipher.java
+++ src/javacore/chapter05/object/project/SubstitutionCipher.java
@@ -2,6 +2,38 @@ package javacore.chapter05.object.project;
 
 public class SubstitutionCipher {
 
+    public static String cipher(String textToEncrypt, String alphabet, String substitutionAlphabet) {
+        char currentChar = ' ';
+        char currentSubChar = ' ';
+        String currentLatinLetter = "";
+        String currentSubLetter = "";
+        String tempEncryptedText = textToEncrypt;
+
+        // first encrypt
+        for (int i = 0; i < alphabet.length(); i++) {
+            currentChar = alphabet.charAt(i);
+
+            currentLatinLetter = "" + currentChar;
+
+            tempEncryptedText = tempEncryptedText.replaceAll(currentLatinLetter, "_" + i + "_");
+
+        }
+
+        String textEncrypted = tempEncryptedText;
+
+        // second encrypt to exclude overlapping replacements
+        for (int i = 0; i < substitutionAlphabet.length(); i++) {
+            currentSubChar = substitutionAlphabet.charAt(i);
+
+            currentSubLetter = "" + currentSubChar;
+
+            textEncrypted = textEncrypted.replaceAll("_" + i + "_", currentSubLetter);
+        }
+
+        textToEncrypt = textEncrypted;
+        return textToEncrypt;
+    }
+
     public static void main(String[] args) {
 
         /**
@@ -19,60 +51,11 @@ public class SubstitutionCipher {
          */
         String textToEncrypt = "ce message secret ne doit pas arriver entre de mauvaises mains !";
 
-
         // Ecrivez le code ci-dessous
-        char currentChar = ' ';
-        char currentSubChar = ' ';
-//
-//        String textEncrypted = "";
-//
-//        // loop text
-//        for (int textIndex = 0; textIndex < textToEncrypt.length(); textIndex++) {
-//            // find each char
-//            currentChar = textToEncrypt.charAt(textIndex);
-//
-//            if (currentChar == ' ' || currentChar == '!') {
-//                currentSubChar = currentChar;
-//            }
-//            else {
-//                int currentCharIndex = 0;
-//                // find char index from latin alphabet
-//                currentCharIndex = latinAlphabet.indexOf(currentChar);
-//                // find sub char with latin alphabet index
-//                currentSubChar = substitutionAlphabet.charAt(currentCharIndex);
-//            }
-//            // Rebuilding of the text from each substitution char
-//            textEncrypted += currentSubChar;
-//        }
-
-
-        String currentLatinLetter = "";
-        String currentSubLetter = "";
-        String newTextToEncrypt = textToEncrypt;
-
-        for (int i = 0; i < latinAlphabet.length(); i++) {
-            currentChar = latinAlphabet.charAt(i);
-
-            currentLatinLetter = "" + currentChar;
-
-            newTextToEncrypt = newTextToEncrypt.replaceAll(currentLatinLetter, "_" + i + "_");
-
-        }
-        // first encrypt
-        System.out.println("text encrypted : " + newTextToEncrypt);
-
-        String textEncrypted = newTextToEncrypt;
-
-        for (int i = 0; i < substitutionAlphabet.length(); i++) {
-            currentSubChar = substitutionAlphabet.charAt(i);
-
-            currentSubLetter = "" + currentSubChar;
-
-            textEncrypted = textEncrypted.replaceAll("_" + i + "_", currentSubLetter);
-        }
-
-        // second encrypt to exclude overlapping replacements
+        String textEncrypted = "";
+        textEncrypted = cipher(textToEncrypt, latinAlphabet, substitutionAlphabet);
         System.out.println("Final encrypted text : " + textEncrypted);
+
     }
 
 }
