5adb4be5d06b9ba1deaaa2906b3a9fc51297c7af
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-04-03 10:22:35 +0200

    first two instructions, create gradesArray with grades inside it with Scanner

---
+++ src/javacore/chapter05/object/exercise/GradesAnalyzer.java
@@ -0,0 +1,43 @@
+package javacore.chapter05.object.exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GradesAnalyzer {
+
+    public static void main(String[] args) {
+
+        System.out.print("Veuillez saisir le nombre de notes que vous souhaitez enregistrer entre 2 et 30 notes : ");
+        try(Scanner scanner = new Scanner(System.in)) {
+            int gradesNumber = 0;
+            if (scanner.hasNextInt()) {
+                gradesNumber = scanner.nextInt();
+                if (gradesNumber < 2 || gradesNumber > 30) {
+                    System.err.print("Vous n'avez pas saisie un nombre de notes valide.");
+                }
+            } else {
+                System.err.print("Il faut saisir un nombre, et pas une autre valeur.");
+            }
+
+            int[] gradesArray = new int[gradesNumber];
+
+
+            System.out.print("Veuillez maintenant saisir les notes que vous voulez enregistrer (entre 0 et 20) : ");
+            for (int gradesIndex = 0; gradesIndex < gradesArray.length; gradesIndex++) {
+                if (scanner.hasNextInt()) {
+                    gradesArray[gradesIndex] = scanner.nextInt();
+                    if (gradesArray[gradesIndex] < 0 || gradesArray[gradesIndex] > 20) {
+                        System.err.print("Vous n'avez pas saisie une note valide.");
+                    }
+                }
+                else {
+                    System.err.print("Il faut saisir un nombre, et pas une autre valeur.");
+                }
+            }
+
+            System.out.println(Arrays.toString(gradesArray));
+        }
+
+    }
+
+}
