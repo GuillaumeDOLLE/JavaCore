ea5652e00977feaaca31e13dbc9e6b0c9b90bb29
Guillaume D <104002464+GuillaumeDOLLE@users.noreply.github.com>
2026-04-20 15:18:04 +0200

    Scanner to let the user type a binary value

--- src/javacore/chapter05/object/exercise/BinaryConverter.java
+++ src/javacore/chapter05/object/exercise/BinaryConverter.java
@@ -1,12 +1,39 @@
 package javacore.chapter05.object.exercise;
 
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class BinaryConverter {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer un nombre binaire (composé uniquement de 0 et de 1, ayant maximum 64 bits) :");
+        String binaryTextEntry = scanner.nextLine();
+        if (isValid(binaryTextEntry)) {
+            BigInteger result = convertBinaryToDecimal(binaryTextEntry);
 
-        String binaryText = "1011001010110111101011001001011010101001011111010111001010101101";
+            System.out.println("binaryText (" + binaryTextEntry + ") in decimal is equal to : " + result);
+        }
+        else {
+            System.out.println("Ce n'était pas un nombre binaire, dommage.");
+        }
+
+        // 3. Je vérifie grâce à un convertisseur avec une recherche internet, ou alors je peux utiliser la méthode interdite pour vérifier.
+
+        // La méthode interdite ne fonctionne pas avec BigInteger.
+
+        // J'avais un problème de cast (long) sur mon Math.pow qui me retirait 1 sur mon result car je dépassais la valeur maximale d'un nombre long
+        // J'avais aussi indexToPow > 0 et non pas égal donc je perdais 1 sur le dernier bit.
+
+
+
+    }
+
+    public static boolean isValid(String userBinaryTextEntry) {
+        return userBinaryTextEntry.matches("[0-1]{1,64}");
+    }
+
+    public static BigInteger convertBinaryToDecimal(String binaryText) {
         BigInteger result = BigInteger.valueOf(0);
         int index = 0;
         for (int indexToPow = binaryText.length() - 1; indexToPow >= 0; indexToPow--) {
@@ -18,16 +45,7 @@ public class BinaryConverter {
             index++;
         }
 
-        System.out.println("binaryText (" + binaryText + ") in decimal is equal to : " + result);
-
-
-        // 3. Je vérifie grâce à un convertisseur avec une recherche internet, ou alors je peux utiliser la méthode interdite pour vérifier.
-
-        // La méthode interdite ne fonctionne pas avec BigInteger.
-
-        // J'avais un problème de cast (long) sur mon Math.pow qui me retirait 1 sur mon result car je dépassais la valeur maximale d'un nombre long
-        // J'avais aussi indexToPow > 0 et non pas égal donc je perdais 1 sur le dernier bit.
-
+        return result;
     }
 
 }
